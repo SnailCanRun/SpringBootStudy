e73cf721b26dc4be634da16e52fef2eed01b8e26
SnailCanRun <zhouyiqiucn@163.com>
2021-01-27 10:32:19 +0800

    remove learn_mybatis_plus

--- atguigustudy/learn_mybatis_plus/src/main/java/com/snailcanrun/mybatisx/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.snailcanrun.mybatisx.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.snailcanrun.mybatisx.entity.User;
-
-public interface UserMapper extends BaseMapper<User> {
-}
--- atguigustudy/learn_mybatis_plus/src/main/java/com/snailcanrun/mybatisx/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.snailcanrun.mybatisx.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.snailcanrun.mybatisx.entity.User;
-
-public interface UserService extends IService<User> {
-}
